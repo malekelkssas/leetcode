f84b04cc810aa7b86f9892f731db8b304384fb3e
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2022-08-27 13:37:44 +0200

    Time: 441 ms (59.02%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-select firstName , lastName , city ,state
-from Person left join Address on Person.personId =Address.personId
+select Person.firstName ,Person.lastName ,Address.city,Address.state  from Person
+left join Address on Person.personId = Address.personId
+order by Person.lastName
+
